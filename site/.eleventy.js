f90e7e7364e9d740dc122029baab1336c4e491e8
PEZ <>
2025-05-13 21:25:56 +0200

    Include the image...

--- site/.eleventy.js
+++ site/.eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   // Copy static assets to the output folder
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/yolomcp-comparison-table.png");
 
   // Custom watch targets
   eleventyConfig.addWatchTarget("src/css/");
